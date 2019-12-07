572e6f34091c447ee9c717c54ec08c00657273c0
Philip Degtyarenko <philimonov199635@gmail.com>
2019-12-07 22:28:12 +0200

    some

---
+++ src/main/java/com/example/OnlineShop/mobel/Customer.java
@@ -0,0 +1,11 @@
+package com.example.OnlineShop.mobel;
+
+import java.sql.Date;
+
+public class Customer {
+
+    private Date birthday;
+    private String firstName;
+    private String lastName;
+
+}
---
+++ src/main/java/com/example/OnlineShop/mobel/User.java
@@ -0,0 +1,9 @@
+package com.example.OnlineShop.mobel;
+
+public class User {
+
+    private long id;
+    private String email;
+    private String password;
+    private boolean active=false;
+}
